7a6469186050e0cce05f29622197d348a7d5097c
Arya R N <63279825+ranilakshmi@users.noreply.github.com>
2024-09-28 18:19:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
+import requests
 
 st.title("ConsumeWise")
 st.text(
     "This application has been created as part of the Gen AI Exchange Hackathon by Google"
 )
 
-st.text_input("Ingredients")
+ingredients = st.text_input("Ingredients")
+if st.button("Submit"):
+    if ingredients:
+        st.write("Ingredients given :\n",ingredients)
