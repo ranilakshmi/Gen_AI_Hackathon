ecbacfce3b7979d2ae492bd37c1fbfc3b7d8d9a5
Arya R N <63279825+ranilakshmi@users.noreply.github.com>
2024-09-28 18:08:07 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("ConsumeWise")
+st.text(
+    "This application has been created as part of the Gen AI Exchange Hackathon by Google"
+)
+
+st.text_input("Ingredients")
